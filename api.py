814bc69b75dd2191df1227ce69875bee7fbcd6af
Gary Bryan <gary.bryan@mercurytide.co.uk>
2019-05-06 14:09:22 +0000

    Offset and limit request parameters

--- api.py
+++ api.py
@@ -18,6 +18,8 @@ LOWERCASE_STORES = [{key: val.lower() for key, val in store.items()} for store i
 
 parser = reqparse.RequestParser()
 parser.add_argument('search')
+parser.add_argument('offset', type=int)
+parser.add_argument('limit', type=int)
 
 
 def search_stores(search):
@@ -37,11 +39,23 @@ class Stores(Resource):
     def get(self):
         args = parser.parse_args()
         search = args['search']
+        any_remaining = False
         if args['search']:
             stores = search_stores(search)
         else:
             stores = STORES
-        return stores
+        if args['offset'] is not None or args['limit'] is not None:
+            offset = args['offset'] or 0
+            limit = offset + args['limit'] if args['limit'] is not None else None
+            limit_slice = slice(offset, limit)
+            num_stores = len(stores) - offset - limit
+            stores = stores[limit_slice]
+            if limit is not None:
+                any_remaining = num_stores > limit
+        return {
+            'stores': stores,
+            'anyRemaining': any_remaining,
+        }
 
 
 api.add_resource(Stores, '/stores')
